a3fb62f47a474cc2cf6af38d532444967e6116be
Dhirajkr08 <dhirajkr1117@gmail.com>
2023-02-21 22:45:18 +0530

    some editing

---
+++ 2nd.js
@@ -0,0 +1,15 @@
+var header=document.querySelector('#main-header');
+header.getElementsByClassName.borderBottom='solid 4px #000';
+
+//var input=document.querySelector('input');
+//input.value='hello world'
+
+var item=document.getElementsByClassName('list-group-item-active');
+console.log(item)
+item[0].textContent="hello World!";
+item[0].style.fontWeight='bold'
+item[0].style.backgroundColor='red'
+
+var item=document.getElementsByTagName('li');
+console.log(item)
+item[4].textContent="i am Bunny"
---
+++ indexx.js
@@ -0,0 +1,8 @@
+//console.log(document)
+//console.log(document.getElementById)
+//var item=documents.getElementById('item1')
+//headerTitle.style.borderbottom="solid3px #000";
+//var input= document.querySelector('input');
+//input.value('hello world')
+console.log("hello world")
+
